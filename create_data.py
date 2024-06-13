889a1117e5a35cee58b4bd6edea67f63d770bb27
pzi <przemek.ziolkowski6@gmail.com>
2024-06-13 18:15:11 +0200

    refactor: move variables to settings

--- create_data.py
+++ create_data.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import numpy as np
 from faker import Faker
+from settings import currencies, sources
 
-fake = Faker()
+def create_dummy_data(file_path = 'dummy_trade_deals.csv', currencies = currencies, sources = sources):
+    fake = Faker()
 
-num_rows = 100
-currencies = ['USD', 'EUR', 'AUD']
-sources = ['FreeCurrencyAPI', 'ECB']
+    num_rows = 20
 
-data = {
-    'deal_currency': np.random.choice(currencies, num_rows),
-    'value': np.round(np.random.uniform(1000, 100000, num_rows), 2),
-    'source': np.random.choice(sources, num_rows),
-    'date_of_transaction': [fake.date_between(start_date='-1y', end_date='today') for _ in range(num_rows)]
-}
 
-trade_deals_df = pd.DataFrame(data)
+    data = {
+        'deal_currency': np.random.choice(currencies, num_rows),
+        'value': np.round(np.random.uniform(1000, 100000, num_rows), 2),
+        'source': np.random.choice(sources, num_rows),
+        'date_of_transaction': [fake.date_between(start_date='-1d', end_date='today') for _ in range(num_rows)]
+    }
 
-file_path = 'dummy_trade_deals.csv'
-trade_deals_df.to_csv(file_path, index=False)
+    trade_deals_df = pd.DataFrame(data)
 
-print(f"Dummy dataset saved to {file_path}")
+    trade_deals_df.to_csv(file_path, index=False)
+
+    print(f"Dummy dataset saved to {file_path}")
